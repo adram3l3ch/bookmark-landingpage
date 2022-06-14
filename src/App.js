09d701a28dd0b14f08b8c496e4d5c793df98aef4
adarsh <adramelech.psd@gmail.com>
2022-06-14 11:13:00 +0530

    hero comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import HERO from "../../images/illustration-hero.svg";
+import "./style.css";
+
+const Hero = () => {
+	return (
+		<div className="hero">
+			<div className="container">
+				<div className="round-bg" />
+				<div className="hero__content">
+					<h1 className="hero__title">A Simple Bookmark Manager</h1>
+					<p className="hero__desc">
+						A clean and simple interface to organize your favourite websites.
+						Open a new browser tab and see your sites load instantly. Try it for
+						free.
+					</p>
+					<div className="hero__btns">
+						<button className="btn primary">Get it on Chrome</button>
+						<button className="btn secondary">Get it on Firefox</button>
+					</div>
+				</div>
+				<div className="hero__image">
+					<img src={HERO} alt="hero-image" aria-hidden />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
